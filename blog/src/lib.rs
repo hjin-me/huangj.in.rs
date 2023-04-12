0ffcb17441c4007367c1b39ce9468962c80e485d
HJin.me <hjin.me@qq.com>
2023-04-12 22:24:09 +0800

    refactor: 重新组织代码结构减少 cfg 编译配置

--- blog/src/lib.rs
+++ blog/src/lib.rs
@@ -1,26 +1,22 @@
-use cfg_if::cfg_if;
 use leptos::*;
+pub mod api;
 pub mod backend;
 pub mod components;
 pub mod error_template;
 pub mod errors;
+#[cfg(feature = "ssr")]
 pub mod fallback;
 
-// Needs to be in lib.rs AFAIK because wasm-bindgen needs us to be compiling a lib. I may be wrong.
-cfg_if! {
-    if #[cfg(feature = "hydrate")] {
-        use wasm_bindgen::prelude::wasm_bindgen;
-        use components::home::*;
+use components::home::*;
+use wasm_bindgen::prelude::wasm_bindgen;
 
-        #[wasm_bindgen]
-        pub fn hydrate() {
-            console_error_panic_hook::set_once();
-            // _ = console_log::init_with_level(log::Level::Debug);
-            console_error_panic_hook::set_once();
+#[wasm_bindgen]
+pub fn hydrate() {
+    console_error_panic_hook::set_once();
+    // _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
 
-            leptos::mount_to_body(|cx| {
-                view! { cx,  <BlogApp/> }
-            });
-        }
-    }
+    mount_to_body(|cx| {
+        view! { cx,  <BlogApp/> }
+    });
 }
